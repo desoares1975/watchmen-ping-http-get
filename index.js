55cec32299a0e51f81f6c8b2e2f8668bab001d0e
Fabio Dias Soares <fabio@bitloom.net>
2016-11-04 17:12:57 -0200

    complete

---
+++ index.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const PingService = function () {};
+
+PingService.prototype.ping = (service, callback) => {
+    let startTime = +new Date();
+    request.get({
+        method: 'GET',
+        uri: service.uri
+    }, (err, response, body) => {
+        return callback(err, body, response, +new Date() - startTime);
+    });
+}
+
+PingService.prototype.getDefaultOptions = () => {
+    return {};
+}
+
+module.exports = PingService;
